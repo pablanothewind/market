e6b7ea7b4d1f5200776654f9d8008dafa1f1d20e
pablanothewind <127697632+pablanothewind@users.noreply.github.com>
2023-03-14 08:55:00 -0400

    Update wsgi.py

--- market/market/wsgi.py
+++ market/market/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'market.settings')
-
+sys.path.append('C:\Users\Amburn\Desktop\market')
 application = get_wsgi_application()
