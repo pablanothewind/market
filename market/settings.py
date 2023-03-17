e588008f9a86a1df9787c428217f313893cff2ab
pablanothewind <127697632+pablanothewind@users.noreply.github.com>
2023-03-16 22:18:01 -0400

    Update settings.py

--- market/settings.py
+++ market/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'market.myapp.apps.myappConfig',
+    'myapp',
 ]
 
 MIDDLEWARE = [
