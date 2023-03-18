a11118f6253a67d79aceb77e5cb1514d338c2716
pablanothewind <127697632+pablanothewind@users.noreply.github.com>
2023-03-18 11:44:03 -0400

    Update settings.py

--- market/settings.py
+++ market/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost", "amburnsmkt.com", "www.amburnsmkt.com", 'sea-lion-app-fzr3t.ondigitalocean.app').split(",")
 
 # Application definition
 
